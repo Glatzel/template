84cd305890bbe2a5b32eccef5ab5e923c2ac2df1
Glatzel <893016099@qq.com>
2025-02-16 11:48:57 +0800

    fix

---
+++ rust/scripts/cargo-clean.ps1
@@ -0,0 +1,3 @@
+Set-Location $PSScriptRoot
+Set-Location ..
+cargo clean
--- rust/scripts/cargo-clippy.ps1
+++ rust/scripts/cargo-clippy.ps1
@@ -5,6 +5,5 @@ if ($env:CI) {
     cargo clippy --all-targets --all-features
 }
 else {
-    & $PSScriptRoot/set-env.ps1
     cargo clippy --fix --all-targets --all-features 
 }
