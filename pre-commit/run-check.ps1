7e5708e32f234b298e505a020c3ea0bd53c10611
Glatzel <893016099@qq.com>
2025-05-07 21:43:37 +0800

    fix

--- pre-commit/run-check.ps1
+++ pre-commit/run-check.ps1
@@ -1,4 +1,6 @@
 param([string[]]$files)
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 Set-Location $PSScriptRoot/..
 foreach ($f in $files) {
     pixi run --manifest-path ./pre-commit/pixi.toml pwsh -f $f
