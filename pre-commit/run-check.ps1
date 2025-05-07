c9ad17a9da7521a714441320cc87a7ce63fa8695
Glatzel <893016099@qq.com>
2025-05-07 19:46:29 +0800

    fix

--- pre-commit/run-check.ps1
+++ pre-commit/run-check.ps1
@@ -1,5 +1,5 @@
 param($name)
 Set-Location $PSScriptRoot/..
-foreach ($f in Get-ChildItem "**/scripts/$name.ps1") {
-    pwsh -f $f
+foreach ($f in Get-ChildItem "**/scripts/$name.ps1" -Recurse) {
+    pixi run --manifest-path ./pre-commit/pixi.toml pwsh -f $f
 }
