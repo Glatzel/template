59fd7506079feca32ce09772cd6d8a8f62e6dc2c
Glatzel <893016099@qq.com>
2025-05-08 20:12:13 +0800

    fix

--- pre-commit/pre-commit.ps1
+++ pre-commit/pre-commit.ps1
@@ -7,12 +7,13 @@ New-Item ./pre-commit -ItemType Directory -ErrorAction SilentlyContinue
 if ($update) {
     Remove-Item ./pre-commit -Recurse -Force -ErrorAction SilentlyContinue
     New-Item ./pre-commit -ItemType Directory -ErrorAction SilentlyContinue
-    curl -L -o ./pre-commit/pixi.lock  https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pixi.lock
-    curl -L -o ./pre-commit/pixi.toml  https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pixi.toml
-    curl -L -o ./pre-commit/pre-commit.ps1  https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pre-commit.ps1
-    curl -L -o ./pre-commit/ruff.toml  https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/ruff.toml
-    curl -L -o ./pre-commit/rustfmt.toml  https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/rustfmt.toml
-    curl -L -o .pre-commit-config.yaml  https://raw.githubusercontent.com/Glatzel/template/main//.pre-commit-config.yaml
+    aria2c -c -x16 -s16 -Z -d ./pre-commit `
+        "https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pixi.lock" `
+        "https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pixi.toml" `
+        "https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/pre-commit.ps1" `
+        "https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/ruff.toml" `
+        "https://raw.githubusercontent.com/Glatzel/template/main//pre-commit/rustfmt.toml"
+    aria2c -c -x16 -s16 -d ./ https://raw.githubusercontent.com/Glatzel/template/main//.pre-commit-config.yaml
     exit 0
 }
 pixi install --manifest-path ./pre-commit/pixi.toml
