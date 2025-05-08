63ae52e2ae7791b51cea9dff1459bb183f589036
Glatzel <893016099@qq.com>
2025-05-08 08:50:20 +0800

    fix

--- pre-commit/run-check.ps1
+++
@@ -1,7 +0,0 @@
-param([string[]]$files)
-Set-Location $PSScriptRoot/..
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
-foreach ($f in $files) {
-    pwsh -f $f
-}
