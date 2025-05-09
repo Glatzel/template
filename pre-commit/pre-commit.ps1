4df5bb70e734ada8b1661e66e8c15b544083a91f
Glatzel <893016099@qq.com>
2025-05-09 11:05:20 +0800

    fix

--- pre-commit/pre-commit.ps1
+++ pre-commit/pre-commit.ps1
@@ -1,8 +1,3 @@
 param ([switch]$update)
 Set-Location $PSScriptRoot/..
-if ($update) {
-    aria2c -c -x16 -s16 -Z -d ./pre-commit `
-        "https://raw.githubusercontent.com/Glatzel/template/main/pre-commit/pixi.toml" `
-        "https://raw.githubusercontent.com/Glatzel/template/main/pre-commit/pixi.lock"
-}
-pixi install --manifest-path ./pre-commit/pixi.toml
+pixi run --manifest-path ./pre-commit/pixi.toml pre-commit-run --color=always --show-diff-on-failure
