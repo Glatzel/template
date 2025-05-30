75f919282e1f3a4a5110a4a67b6a0b3d8c9a1186
Glatzel <893016099@qq.com>
2025-05-30 13:39:00 +0800

    f

--- rust/scripts/cargo-bench.ps1
+++ rust/scripts/cargo-bench.ps1
@@ -1,5 +1,7 @@
 param($filter)
-
+if (Test-Path $PSScriptRoot/setup.ps1) {
+    &$PSScriptRoot/setup.ps1
+}
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 cargo bench --all -- $filter
