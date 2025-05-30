149f37222109efa963c0f51866518247ad8e8716
Glatzel <893016099@qq.com>
2025-05-30 13:37:13 +0800

    f

--- rust/scripts/cargo-bench.ps1
+++ rust/scripts/cargo-bench.ps1
@@ -1,4 +1,7 @@
+param($filter)
+
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
-cargo clean
+cargo bench --all -- $filter
+Set-Location $PSScriptRoot
 Set-Location $ROOT
--- rust/scripts/cargo-test.ps1
+++ rust/scripts/cargo-test.ps1
@@ -1,13 +1,39 @@
-Set-Location $PSScriptRoot
-Set-Location ..
+if (Test-Path $PSScriptRoot/setup.ps1) {
+    &$PSScriptRoot/setup.ps1
+}
+$ROOT = git rev-parse --show-toplevel
+Set-Location $PSScriptRoot/..
+git submodule update --init --recursive
+pixi install
 
-cargo +nightly llvm-cov --no-report --all-features --workspace nextest
-cargo +nightly llvm-cov --no-report --all-features --workspace --doc
+Write-Output "::group::nextest"
+cargo +nightly llvm-cov nextest --no-report --all --branch --no-fail-fast
+$code = $LASTEXITCODE
+Write-Output "::endgroup::"
 
+Write-Output "::group::doctest"
+cargo +nightly llvm-cov --no-report --all $package --branch --no-fail-fast --doc
+$code = $code + $LASTEXITCODE
+Write-Output "::endgroup::"
+
+Write-Output "::group::report"
+cargo +nightly llvm-cov report
+Write-Output "::endgroup::"
+
+Write-Output "::group::lcov"
 if ( $env:CI ) {
-    cargo +nightly llvm-cov report
     cargo +nightly llvm-cov report --lcov --output-path lcov.info
 }
-else {
-    cargo +nightly llvm-cov report
+Write-Output "::endgroup::"
+
+Write-Output "::group::result"
+$code = $code + $LASTEXITCODE
+if ($code -ne 0) {
+    Write-Host "Test failed." -ForegroundColor Red
 }
+else {
+    Write-Host "Test succeeded." -ForegroundColor Green
+}
+Write-Output "::endgroup::"
+Set-Location $ROOT
+exit $code
