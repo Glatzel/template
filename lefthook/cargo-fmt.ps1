a55b98942de8bb700e5fcfcf174d3966c6ffbc41
Glatzel <893016099@qq.com>
2025-08-22 19:15:27 +0800

    f

--- lefthook/cargo-fmt.ps1
+++ lefthook/cargo-fmt.ps1
@@ -1,13 +1,12 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
-param($files)
 if ($env:CI) {
     rustup toolchain install nightly --profile=minimal
     rustup component add rustfmt --toolchain nightly
 }
 $ROOT = git rev-parse --show-toplevel
 Set-Location $ROOT
-foreach ($file in $files) {
+foreach ($file in $args) {
     Set-Location (Split-Path (Resolve-Path file) -Parent)
     Write-Output "Cargo fmt in: $pwd"
     cargo +nightly fmt --all
--- lefthook/cargo-machete.ps1
+++ lefthook/cargo-machete.ps1
@@ -1,11 +1,10 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
-param($files)
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $ROOT
-foreach ($file in $files) {
+foreach ($file in $args) {
     Set-Location (Split-Path (Resolve-Path file) -Parent)
     Write-Output "Cargo machete in: $pwd"
     cargo machete
