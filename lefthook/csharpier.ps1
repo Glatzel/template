1b6106ffa55c6dfd20d49c43714d144707accc03
Glatzel <893016099@qq.com>
2025-08-23 17:20:29 +0800

    f

--- lefthook/csharpier.ps1
+++ lefthook/csharpier.ps1
@@ -1,7 +1,8 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
+if (-not $args) { exit 0 }
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
 Set-Location $ROOT
-if ($args) { dotnet tool install -g csharpier; csharpier format .; csharpier check . }
+dotnet tool install -g csharpier; csharpier format .; csharpier check .
