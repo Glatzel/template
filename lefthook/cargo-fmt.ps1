49ce11b9b0ef8b20f4c2344abe62dc37d120e252
Glatzel <893016099@qq.com>
2025-08-29 13:30:28 +0800

    分

--- lefthook/cargo-fmt.ps1
+++ lefthook/cargo-fmt.ps1
@@ -6,10 +6,9 @@ if ($env:CI) {
     rustup toolchain install nightly --profile=minimal
     rustup component add rustfmt --toolchain nightly
 }
-$ROOT = git rev-parse --show-toplevel
-Set-Location $ROOT
 foreach ($file in $args) {
     Set-Location (Split-Path (Resolve-Path $file) -Parent)
     Write-Output "Cargo fmt in: $pwd"
     cargo +nightly fmt --all --quiet
+    Set-Location $ROOT
 }
