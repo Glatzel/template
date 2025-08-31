27d30799ef6bca78ff2280c32d2f4bc9bff832c1
Glatzel <893016099@qq.com>
2025-08-31 11:23:51 +0800

    f

--- rust/scripts/cargo-bench.ps1
+++ rust/scripts/cargo-bench.ps1
@@ -1,6 +1,6 @@
 # This File is automatically synchronized from https://github.com/Glatzel/template
 
-$config = if (-not $args) { '--all' } else { $args }
+$config = if (-not $args) { '--workspace' } else { $args }
 
 if (Test-Path $PSScriptRoot/setup.ps1) { &$PSScriptRoot/setup.ps1 }
 $ROOT = git rev-parse --show-toplevel
