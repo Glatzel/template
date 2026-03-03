f47b72a03ede247f2c72b73593c2322056d06d82
Glatzel <chenzelin9794@gmail.com>
2026-03-03 19:48:42 +0800

    f

--- rust/scripts/cargo-test.ps1
+++ rust/scripts/cargo-test.ps1
@@ -3,7 +3,6 @@
 $config = if ($args.Count) { $args } else { @('--workspace', '--all-features') }
 if (Test-Path $PSScriptRoot/setup.ps1) { &$PSScriptRoot/setup.ps1 }
 if ($env:CI) { pixi global install cargo-llvm-cov cargo-nextest }
-$ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot/..
 
 Write-Output "::group::nextest"
