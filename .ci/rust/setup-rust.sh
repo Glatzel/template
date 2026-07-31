ce7ed5a7e293ae2ab18ad90b4403a2eecf4d7b6d
Glatzel <chenzelin9794@gmail.com>
2026-07-31 14:41:04 +0800

    f

--- .ci/rust/setup-rust.sh
+++ .ci/rust/setup-rust.sh
@@ -19,7 +19,6 @@ else
     rustup toolchain install "$TOOLCHAIN" --profile minimal
     rustup default "$TOOLCHAIN"
 fi
-. "$HOME/.cargo/env"
 if [ -n "$COMPONENTS" ]; then
     rustup component add $COMPONENTS
 fi
