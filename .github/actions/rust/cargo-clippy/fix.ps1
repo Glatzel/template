06f38d8726eaef3e8739b0c710177f05b027b424
Glatzel <chenzelin9794@gmail.com>
2026-08-02 20:50:44 +0800

    Update fix.ps1

--- .github/actions/rust/cargo-clippy/fix.ps1
+++ .github/actions/rust/cargo-clippy/fix.ps1
@@ -1,2 +1,4 @@
+# This file is automatically synchronized from https://github.com/Glatzel/template
+
 $clippy_args = $(yq '.inputs.args.default' "$PSScriptRoot/action.yml") -split ' '
 cargo clippy --fix --allow-dirty --workspace -- $clippy_args
