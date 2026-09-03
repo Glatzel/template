a9ec20249b5cfcf8f1562ef61f8a5994f86ab92a
Glatzel <chenzelin9794@gmail.com>
2026-09-03 17:05:39 +0800

    Update create-tag.ps1

--- create-tag.ps1
+++ create-tag.ps1
@@ -3,13 +3,13 @@ $tag="v$version"
 if($(gh api /repos/Glatzel/template/tags --jq '.[].name') -contains "$tag")
 {
     write-output "tag $tag exists."
-    gh release delete $tag --cleanup-tag -y
+    git push origin --delete $tag
 } else
 {
     write-output "tag $tag does not exist."
 }
-if (git tag --list "<tag-name>") {
-    git tag -d "<tag-name>"
+if (git tag --list "$tag") {
+    git tag -d "$tag"
 }
 git tag -a $tag -m "add tag $tag"
 git push origin $tag
