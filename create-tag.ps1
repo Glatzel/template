c98af6e24c8b23949695c7c4640dc4f8d6b252dd
Glatzel <chenzelin9794@gmail.com>
2026-09-03 16:58:32 +0800

    f

--- create-tag.ps1
+++ create-tag.ps1
@@ -1,6 +1,16 @@
-$build=30
-$version = Get-Date -Format 'yyyy.M'
-$tag="v$version.$build"
+$version = Get-Date -Format 'yyyy.M.d'
+$tag="v$version"
+if($(gh api /repos/Glatzel/template/tags --jq '.[].name') -contains "$tag")
+{
+    write-output "tag $tag exists."
+    gh release delete $tag --cleanup-tag -y
+} else
+{
+    write-output "tag $tag does not exist."
+}
+if (git tag --list "<tag-name>") {
+    git tag -d "<tag-name>"
+}
 git tag -a $tag -m "add tag $tag"
 git push origin $tag
 pinact run -u
